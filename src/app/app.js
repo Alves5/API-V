5e05f07fa4f2e81ed54a68930fd04543690a03f3
Daniel Rodrigues Alves <drodriguesalves2@gmail.com>
2023-07-06 15:38:05 -0300

    Update para dev

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,9 @@
 import express from "express";
 import routes from "./routes.js";
-import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from '../config/swagger.json' assert { type: 'json' };
 
 const app = express();
-
-app.use((req, res, next) => {
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    //Quais são os métodos que a conexão pode realizar na API
-    res.setHeader("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.setHeader("Access-Control-Allow-Headers", "Content-type")
-    app.use(cors());
-    next();
-});
 
 app.use(express.json());
 
