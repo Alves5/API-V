a1cfd0947e781dd8ed1c5b3b8e98b6570871a08f
Daniel Rodrigues Alves <da691985@gmail.com>
2023-08-28 10:34:04 -0300

    Ajustes de branch noSql

---
+++ src/config/database/connectionMongoDb.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import contatoSchema from "../../app/model/modelMongoDb/Contato.js";
+
+
+//configrações nosql
+const connectionMongoDb =  mongoose.connect('mongodb://127.0.0.1:27017/db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//conectando ao banco de dados nosql
+connectionMongoDb.then(function(db) {
+    console.log('MongoDB conectado');
+}).catch(function(err) {
+    console.log('Erro ao conectar ao MongoDB: ' + err);
+});
+
+//criando a collection
+mongoose.model('Contato', contatoSchema);
+
+export default connectionMongoDb;
