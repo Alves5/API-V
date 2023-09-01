08061e54aefe9e15b6dfe56d2c17efe272f3cace
Denilson <denilsonrabelo.dev@gmail.com>
2023-09-01 00:06:29 -0300

    ft-dev-denilson-1

--- src/app/model/modelMongoDb/Contato.js
+++ src/app/model/modelMongoDb/Contato.js
@@ -3,6 +3,16 @@ import { Schema } from 'mongoose';
 
 const contatoSchema = new mongoose.Schema({
     id_empresa: Number,
+    status: String,
+    numeroContato: String,
+    numeroProposta: String,
+    documentoCliente: String,
+    seguroViajante: String,
+    visto: String,
+    acomodacao: String,
+    translado: String,
+    criadoPor: String,
+    contratoPai: String,
     camposAdicionais: mongoose.Schema.Types.Mixed,
 }, { collection: 'Contato' }, { timestamps: true }, { versionKey: false }, { _id: false }, { strict: false });
 
